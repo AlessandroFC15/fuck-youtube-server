3ce5c719ddadd6a230718c5c1276c00bc2c2aedc
Alessandrø <ale-remo@hotmail.com>
2019-08-21 02:08:31 -0300

    Handle request errors on TubeUnblock service

--- src/services/tubeUnblock/index.test.js
+++ src/services/tubeUnblock/index.test.js
@@ -12,8 +12,8 @@ const validEmbedResponse = getMockResponse('/tubeUnblock/embedResponse.html')
 describe('extract mirrors from TubeUnblock page', () => {
   describe('successful scenarios', () => {
     beforeEach(() => {
-      getMainPageResponse.mockResolvedValue({ data: validMainPageResponse })
-      getResponseFromEmbedEndpoint.mockResolvedValue({ data: validEmbedResponse })
+      getMainPageResponse.mockResolvedValue(validMainPageResponse)
+      getResponseFromEmbedEndpoint.mockResolvedValue(validEmbedResponse)
     })
 
     it('should return video source starting with "https://tubeunblock.com/video/"', () => {
@@ -26,14 +26,27 @@ describe('extract mirrors from TubeUnblock page', () => {
 
   describe('unsuccessful scenarios', () => {
     it('should return null when iframe src is not found on TubeUnblock page', () => {
-      getMainPageResponse.mockResolvedValue({ data: '<html>Error Page</html>' })
+      getMainPageResponse.mockResolvedValue('<html>Error Page</html>')
 
       return expect(getVideoSrc('tliR8gHahjc')).resolves.toBe(null)
     })
 
     it('should return null when second request (sent to /embed) returns page without video source', () => {
-      getMainPageResponse.mockResolvedValue({ data: validMainPageResponse })
-      getResponseFromEmbedEndpoint.mockResolvedValue({ data: '<html>Error Page</html>' })
+      getMainPageResponse.mockResolvedValue(validMainPageResponse)
+      getResponseFromEmbedEndpoint.mockResolvedValue('<html>Error Page</html>')
+
+      return expect(getVideoSrc('tliR8gHahjc')).resolves.toBe(null)
+    })
+
+    it('should return null when request to main page fails', () => {
+      getMainPageResponse.mockResolvedValue(null)
+
+      return expect(getVideoSrc('tliR8gHahjc')).resolves.toBe(null)
+    })
+
+    it('should return null when request to embed endpoint fails', () => {
+      getMainPageResponse.mockResolvedValue(validMainPageResponse)
+      getResponseFromEmbedEndpoint.mockResolvedValue(null)
 
       return expect(getVideoSrc('tliR8gHahjc')).resolves.toBe(null)
     })
