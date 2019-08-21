997d8a5881baf0f075d652f0fc2d3a0cfe045bea
Alessandrø <ale-remo@hotmail.com>
2019-08-20 23:42:33 -0300

    Create GenYoutube service

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,18 @@
+import puppeteer from 'puppeteer'
+
 export const isYoutubeVideoLink = (url) =>
   (/^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/watch\?(.*)(v=.+)(.*)$/).test(url)
+
+export const getPageHTML = async ({ url, selectorToWaitFor }) => {
+  const browser = await puppeteer.launch()
+
+  const page = await browser.newPage()
+
+  await page.goto(url, { waitUntil: 'domcontentloaded' })
+
+  if (selectorToWaitFor) {
+    await page.waitForSelector(selectorToWaitFor)
+  }
+
+  return page.evaluate(() => document.documentElement.outerHTML)
+}
